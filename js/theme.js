2a9739042d999e52f7a60b966588393cab324145
YuklidD <ict21895@sjp.ac.lk>
2024-05-11 22:23:11 +0530

    new one

--- js/theme.js
+++ js/theme.js
@@ -76,6 +76,38 @@ $('.navbar-side-open [data-toggle="collapse"], .navbar-overlay [data-toggle="col
    $($(this).data('target')).toggleClass('show');
 })
 
+
+/*---------------------------------
+  year and actions
+----------------------------------- */
+
+// JavaScript to update project name when radio button is clicked
+document.addEventListener('DOMContentLoaded', function() {
+    const radioButtons = document.querySelectorAll('.carousel input[name="slides"]');
+    const projectName = document.getElementById('project-name');
+
+    radioButtons.forEach(function(radioButton) {
+        radioButton.addEventListener('click', function() {
+            const projectNameText = this.previousElementSibling.textContent;
+            projectName.textContent = projectNameText;
+        });
+    });
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    // Get the current year
+    var currentYear = new Date().getFullYear();
+
+    // Set the current year in the span element
+    var currentYearSpan = document.getElementById("currentYear");
+    if (currentYearSpan !== null) {
+        currentYearSpan.innerText = currentYear;
+    } else {
+        console.error("Element with ID 'currentYear' not found.");
+    }
+});
+
+
 /*---------------------------------
    Carousel (Owl Carousel)
 ----------------------------------- */
